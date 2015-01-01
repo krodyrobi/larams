361c9c81a138bf63847e385239aabd41ea40b766
Krody Robert <krody.robi@gmail.com>
2015-01-01 13:00:32 +0200

    Fixed seeders

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -2,19 +2,21 @@
 
 class UsersTableSeeder extends Seeder {
 
-  public function run()
-  {
-    $user = new User;
-    $user->username = 'admin';
-    $user->email = 'krody.robi@gmail.com';
-    $user->password = 'admin';
-    $user->password_confirmation = 'admin_1234';
-    $user->confirmation_code = md5(uniqid(mt_rand(), true));
+    public function run() {
+        DB::table('users')->delete();
 
-    if(! $user->save()) {
-      Log::info('Unable to create user '.$user->username, (array)$user->errors());
-    } else {
-      Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        $user = new User;
+        $user->username = 'admin';
+        $user->email = 'krody.robi@gmail.com';
+        $user->password = 'admin';
+        $user->password_confirmation = 'admin';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+        $user->confirmed = 1;
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        };
     }
-  }
 }
