7380d4f56d1b45d87a92fc55721d68306500fce1
Krody Robert <krody.robi@gmail.com>
2015-01-01 13:08:44 +0200

    Added js library deps

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -18,5 +18,11 @@
             @yield('content')
         </div>
         <?php echo '<pre>'.print_r($jsConfig,1).'</pre>'; ?>
+
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone-relational/0.9.0/backbone-relational.min.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone.paginator/0.8/backbone.paginator.min.js"></script>
     </body>
 </html>
