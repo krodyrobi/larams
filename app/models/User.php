566e487a167a8c71718c06035a052e5e1cdd0b06
Krody Robert <krody.robi@gmail.com>
2015-01-02 15:52:19 +0200

    Updated relationships and attribute visibility

--- app/models/User.php
+++ app/models/User.php
@@ -5,14 +5,16 @@ use Zizaco\Confide\ConfideUserInterface;
 use Zizaco\Entrust\HasRole;
 
 
-class User implements ConfideUserInterface {
+class User extends Eloquent implements ConfideUserInterface {
 	use ConfideUser;
 	use HasRole;
+
+    protected $hidden = array('password', 'remember_token', 'confirmation_code');
 
     /*
      * @Relation
      */
     public function posts() {
-        return $this->hasMany('Post');
+        return $this->hasMany('Post', 'author_id');
     }
 }
