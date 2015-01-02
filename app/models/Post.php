e7836742649fa20ce2b74d699ee728e7393b9d6f
Krody Robert <krody.robi@gmail.com>
2015-01-02 15:53:02 +0200

    Added ardent functionality + some relationships

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,12 +2,32 @@
 
 use LaravelBook\Ardent\Ardent;
 
-class Post extends Ardent {
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+
+class Post extends Ardent implements SluggableInterface {
+    use SluggableTrait;
+
+    protected $sluggable = array(
+        'build_from' => 'title'
+    );
+
+
+    public $autoHydrateEntityFromInput = true;
+    public $forceEntityHydrationFromInput = true;
+    public $autoPurgeRedundantAttributes = true;
+
+    public static $rules = array(
+        'title' => 'required',
+        'body'  => 'required'
+    );
+
+    protected $guarded = array('slug');
 
     /*
      * @Relation
      */
     public function author() {
-        return $this->belongsTo('User');
+        return $this->belongsTo('User', 'author_id', 'id');
     }
 } 
