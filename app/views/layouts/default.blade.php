d1f90809f4e1bfb2243c89f0f561f41c08dbdb2f
Krody Robert <krody.robi@gmail.com>
2015-01-03 15:46:53 +0200

    Added bootstrap resources and made sure ajax calls will have the auth_token, csrf_token

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,14 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <script type="text/javascript">
-            $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-              if (config.auth_token) {
-                jqXHR.setRequestHeader('X-Auth-Token', config.auth_token);
-              }
-            });
-        </script>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
     </head>
+
     <body>
         @section('sidebar')
             This is the master sidebar.
@@ -17,12 +17,28 @@
         <div class="container">
             @yield('content')
         </div>
-        <?php echo '<pre>'.print_r($jsConfig,1).'</pre>'; ?>
 
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone-relational/0.9.0/backbone-relational.min.js"></script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone.paginator/0.8/backbone.paginator.min.js"></script>
+
+        <script type="text/javascript">
+            var config = {{ $config }};
+            console.log(config.auth_token);
+
+
+            (function( $ ) {
+                $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+                    if (config.auth_token)
+                        jqXHR.setRequestHeader('X-Auth-Token', config.auth_token);
+                    if (config.csrf_token)
+                        jqXHR.setRequestHeader('_token', config.csrf_token);
+                });
+
+            })( jQuery );
+        </script>
     </body>
 </html>
