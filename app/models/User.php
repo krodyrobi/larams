a04153c307c1ba4b61ad17e7a50e2d1996da6885
Krody Robert <krody.robi@gmail.com>
2015-01-03 15:44:21 +0200

    Added method to retreive permission list for a user IT IS NOT A COLLECTION

--- app/models/User.php
+++ app/models/User.php
@@ -10,11 +10,36 @@ class User extends Eloquent implements ConfideUserInterface {
 	use HasRole;
 
     protected $hidden = array('password', 'remember_token', 'confirmation_code');
+    protected $guarded  = array('id','created_at','updated_at');
 
     /**
      * @Relation
      */
     public function posts() {
         return $this->hasMany('Post', 'author_id');
+    }
+
+
+    public function getPermissions() {
+        $roles = $this->roles()->with('perms')->get()->toArray();
+
+        $perm_groups = [];
+        foreach( $roles as $role ) {
+            $perm_groups[] = $role['perms'];
+        }
+
+        $perm_ids = [];
+        $unique_perms = [];
+        foreach( $perm_groups as $group ) {
+            foreach( $group as $p ) {
+                if (in_array($p['id'], $perm_ids))
+                    continue;
+
+                $perm_ids[] = $p['id'];
+                $unique_perms[] = $p;
+            }
+        }
+
+        return $unique_perms;//$roles['permissions']/array_map( function($el) { return [$el['id'] => $el['name']]; }, $roles['perms']);
     }
 }
