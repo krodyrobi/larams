88b2b83ff694963c9c6bd818c927884b6d87c4eb
Krody Robert <krody.robi@gmail.com>
2015-01-03 16:02:52 +0200

    Minor modifications + todos

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -1,69 +1,72 @@
 <?php
 
 class PostsController extends \BaseController {
+//class PostsController extends ApiController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
         //TODO make this removal of extra fields easier
         //TODO make sure the meta total_count is set: _config=meta-total-count
-        
+
         $input = Input::all();
-        unset($input['auth_token']);
-		$response = ApiHandler::parseMultiple(new Post(), ['title', 'body'], $input);
+        if ( !isset( $input['_config'] ) || $input['_config'] !== 'meta-total-count' )
+            $input['_config'] = 'meta-total-count';
+
+        $response = ApiHandler::parseMultiple(new Post(), ['title', 'body'], $input);
+
+        //TODO make the paging system consistent with other apis
 
         return $response->getResponse();
-	}
+    }
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        $status = Post::create(Input::all());
+
+        dd($status);
+    }
 
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id) {
+        //
+    }
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
 
 
 }
