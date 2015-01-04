277158b2f5f8bfa2edbbef9c459675d5f93d16b5
Krody Robert <krody.robi@gmail.com>
2015-01-04 21:00:41 +0200

    Refactored the posts controller after logic was moved to a generic class

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -1,65 +1,8 @@
 <?php
 
-class PostsController extends \BaseController {
-//class PostsController extends ApiController {
 
-
-    public function index() {
-        //TODO make this removal of extra fields easier
-        //TODO make sure the meta total_count is set: _config=meta-total-count
-
-        $input = Input::all();
-        if ( !isset( $input['_config'] ) || $input['_config'] !== 'meta-total-count' )
-            $input['_config'] = 'meta-total-count';
-
-        $response = ApiHandler::parseMultiple(new Post(), ['title', 'body'], $input);
-
-        //TODO make the paging system consistent with other apis
-
-        return $response->getResponse();
+class PostsController extends ApiBaseController {
+    public function __construct() {
+        parent::__construct(new Post, new PostTransformer(), []);
     }
-
-
-
-    public function store() {
-        $post = new Post;
-        if($post->save())
-            return $post->toJson();
-
-        throw new ValidationException($post->errors());
-    }
-
-
-    public function show($id) {
-        $post = Post::with('author')->find($id);
-        if ( !$post )
-            throw new NotFoundException('Post Not Found');
-
-        return $post->toJson();
-    }
-
-
-    public function update($id) {
-        $post = Post::find($id);
-        if ( !$post )
-            throw new NotFoundException('Post Not Found');
-
-
-        if($post->save())
-            return $post->toJson();
-
-        throw new ValidationException($post->errors());
-    }
-
-
-    public function destroy($id) {
-        $post = Post::find($id);
-        if ( !$post )
-            throw new NotFoundException('Post Not Found');
-
-        $post->delete();
-        return Response::json('', 204);
-    }
-
-
 }
