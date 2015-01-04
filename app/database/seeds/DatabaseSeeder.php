588c84da44bbd0aa02289e32989a974b34ae5344
Krody Robert <krody.robi@gmail.com>
2015-01-04 20:58:23 +0200

    Added apikey seeder and added some more users

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,20 +1,11 @@
 <?php
 
 class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
+	public function run() {
 		Eloquent::unguard();
 
 		$this->call('UsersTableSeeder');
-		$this->call('PermissionsSeeder');
-
+        $this->call('ApiKeysSeeder');
         $this->call('PostsSeeder');
 	}
-
 }
--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,11 +12,24 @@ class UsersTableSeeder extends Seeder {
         $user->password_confirmation = 'admin';
         $user->confirmation_code = md5(uniqid(mt_rand(), true));
         $user->confirmed = 1;
+        $user->save();
 
-        if(! $user->save()) {
-            Log::info('Unable to create user '.$user->username, (array)$user->errors());
-        } else {
-            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
-        };
+        $user = new User;
+        $user->username = 'moderator';
+        $user->email = 'moderator@gmail.com';
+        $user->password = 'moderator';
+        $user->password_confirmation = 'moderator';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+        $user->confirmed = 1;
+        $user->save();
+
+        $user = new User;
+        $user->username = 'viewer';
+        $user->email = 'viewer@gmail.com';
+        $user->password = 'viewer';
+        $user->password_confirmation = 'viewer';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+        $user->confirmed = 1;
+        $user->save();
     }
 }
