d7a2ffaed85c97cc0ab524c7de373732158328bc
Krody Robert <krody.robi@gmail.com>
2015-01-04 20:55:16 +0200

    Removed permissions fetcher

--- app/models/User.php
+++ app/models/User.php
@@ -18,28 +18,4 @@ class User extends Eloquent implements ConfideUserInterface {
     public function posts() {
         return $this->hasMany('Post', 'author_id');
     }
-
-
-    public function getPermissions() {
-        $roles = $this->roles()->with('perms')->get()->toArray();
-
-        $perm_groups = [];
-        foreach( $roles as $role ) {
-            $perm_groups[] = $role['perms'];
-        }
-
-        $perm_ids = [];
-        $unique_perms = [];
-        foreach( $perm_groups as $group ) {
-            foreach( $group as $p ) {
-                if (in_array($p['id'], $perm_ids))
-                    continue;
-
-                $perm_ids[] = $p['id'];
-                $unique_perms[] = $p;
-            }
-        }
-
-        return $unique_perms;//$roles['permissions']/array_map( function($el) { return [$el['id'] => $el['name']]; }, $roles['perms']);
-    }
 }
